3dbd6d0dc8a182a7060badac5f4453a43bfe7c11
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-05-17 14:13:07 -0300

    fix: correct formatting and method calls in ListagemClientes class

--- src/services/listing/listagemClientes.ts
+++ src/services/listing/listagemClientes.ts
@@ -2,19 +2,21 @@ import Cliente from "../../models/cliente";
 import Listagem from "./listagem";
 
 export default class ListagemClientes extends Listagem {
-    private clientes: Array<Cliente>
+    private clientes: Array<Cliente>;
+
     constructor(clientes: Array<Cliente>) {
-        super()
-        this.clientes = clientes
+        super();
+        this.clientes = clientes;
     }
+
     public listar(): void {
-        console.log(`\nLista de todos os clientes:`);
+        console.log(`\nLista de todos os clientes:\n`);
         this.clientes.forEach(cliente => {
-            console.log(`Nome: ` + cliente.nome);
-            console.log(`Nome social: ` + cliente.nomeSocial);
-            console.log(`CPF: ` + cliente.getCpf.getValor);
+            console.log(`Nome: ${cliente.getNome()}`);
+            console.log(`Nome social: ${cliente.getNomeSocial()}`);
+            console.log(`CPF: ${cliente.getCpf().getValor()}`);
             console.log(`--------------------------------------`);
         });
-        console.log(`\n`);
+        console.log();
     }
 }
