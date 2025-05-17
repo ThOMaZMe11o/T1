5d84c97b87b3ffa6de0dabeb3b8d556afb8a039c
ThOMaZMe11o <thomazmello1912@gmail.com>
2025-05-17 13:47:12 -0300

    feat: implement CadastroProduto class for product registration functionality

---
+++ src/services/register/cadastroProduto.ts
@@ -0,0 +1,26 @@
+import Entrada from "../../client/entrada";
+import Produto from "../../models/produto";
+import Cadastro from "../register/cadastro";
+
+export default class CadastroProduto extends Cadastro {
+    private produtos: Produto[];
+    private entrada: Entrada;
+
+    constructor(produtos: Produto[]) {
+        super();
+        this.produtos = produtos;
+        this.entrada = new Entrada();
+    }
+
+    public cadastrar(): void {
+        console.log(`\nCadastro de produto:`);
+
+        const nome = this.entrada.receberTexto(`Informe o nome do produto: `);
+        const preco = this.entrada.receberNumero(`Informe o preço do produto: `);
+
+        const produto = new Produto(nome, preco);
+        this.produtos.push(produto);
+
+        console.log(`✅ Produto cadastrado com sucesso!\n`);
+    }
+}
